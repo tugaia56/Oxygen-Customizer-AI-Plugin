85e244f2e203c51d7118bb2e523d6552d4da91ea
DHD2280 <luigiconte98@gmail.com>
2024-10-18 00:32:06 +0200

    Move Links in Constats

--- app/src/main/java/it/dhd/oxygencustomizer/aiplugin/utils/Constants.java
+++ app/src/main/java/it/dhd/oxygencustomizer/aiplugin/utils/Constants.java
@@ -6,4 +6,8 @@ public final class Constants {
     public static final String ACTION_EXTRACT_SUCCESS = "it.dhd.oxygencustomizer.aiplugin.ACTION_EXTRACT_SUCCESS";
     public static final String ACTION_EXTRACT_FAILURE = "it.dhd.oxygencustomizer.aiplugin.ACTION_EXTRACT_FAILURE";
 
+    // Updates
+    public final static String AI_MODELS = "https://raw.githubusercontent.com/DHD2280/Oxygen-Customizer-AI-Plugin/main/available_models.json";
+    public final static String APP_UPDATES = "https://raw.githubusercontent.com/DHD2280/Oxygen-Customizer-AI-Plugin/main/latestVersion.json";
+
 }
