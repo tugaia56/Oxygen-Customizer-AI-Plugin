e0b8e80f2f2291636c89326827bb862921de004b
GitHub <OxygenCustomizerAIPlugin@dhd.it>
2024-10-17 22:40:29 +0000

    Version update: Release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "it.dhd.oxygencustomizer.aiplugin"
         minSdk = 31
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "2.0.0"
         setProperty("archivesBaseName", "OxygenCustomizerAIPlugin.apk")
     }
 
